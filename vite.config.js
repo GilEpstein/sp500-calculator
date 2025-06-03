b7256651ed246ee27edc710fb6b77f4144072dcc
GilEpstein <81075922+GilEpstein@users.noreply.github.com>
2025-06-03 12:32:39 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/sp500-calculator/', // נדרש ל-GitHub Pages כדי שהקבצים ייטענו נכון
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/sp500-calculator/',
   build: {
     outDir: 'dist',
-    sourcemap: true
+    emptyOutDir: true,
   },
-});
+})
